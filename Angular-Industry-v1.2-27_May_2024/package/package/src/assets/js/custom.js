11bb07951667df55bbadd35c3cbc64a874100fb9
root <root@rudransh3067@gmail.com>
2025-08-03 04:02:02 +0530

    V1.0.0

--- Angular-Industry-v1.2-27_May_2024/package/package/src/assets/js/custom.js
+++ Angular-Industry-v1.2-27_May_2024/package/package/src/assets/js/custom.js
@@ -1029,7 +1029,7 @@ var Industry = function () {
   }
 
   var handleSupport = function () {
-    var support = '<a href="https://1.envato.market/mWLka" target="_blank" class="bt-buy-now theme-btn"><i class="ti-shopping-cart"></i><span>Buy Now</span></a><a href="https://support.w3itexperts.com" target="_blank" class="bt-support-now theme-btn"><i class="ti-headphone-alt"></i><span>Support</span></a><!-- Go to www.addthis.com/dashboard to customize your tools --><script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b221c5e31b4e54b"></script>';
+    //var support = '<a href="https://1.envato.market/mWLka" target="_blank" class="bt-buy-now theme-btn"><i class="ti-shopping-cart"></i><span>Buy Now</span></a><a href="https://support.w3itexperts.com" target="_blank" class="bt-support-now theme-btn"><i class="ti-headphone-alt"></i><span>Support</span></a><!-- Go to www.addthis.com/dashboard to customize your tools --><script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5b221c5e31b4e54b"></script>';
     jQuery('body').append(support);
   }
 
